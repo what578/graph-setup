0d336982287cc5a46e828f31153107f9a1086e47
what578 <ryan_merge@hotmail.com>
2023-01-27 05:38:21 -0600

    completed legend class

--- legend.py
+++ legend.py
@@ -1,4 +1,58 @@
 import column
+import numpy
+import errorRaiser
 
 
 class Legend:
+    columns         = None
+    legend          = None
+    def __init__(self,columns):
+        self.setColumns(columns)
+    def setColumns(self,columns):
+        errorRaiser.typeError("setColumns",columns,type([4]))
+        if len(columns) == 0:
+            raise Valuerror("invalid number of columns")
+        for x in columns:
+            errorRaiser.typeError("setColumns",x,type(column.Column(5,"t")))
+            self.columns = columns
+    def createLegend(self):
+        largestStr = self.largestStr()
+        legend     = []
+        legend.append(self.creatBotOfLegend(largestStr))
+        legend = self.createCoreOfLegend(legend,largestStr)
+        legend.append(self.createTopofLegend(largestStr))
+        self.legend=legend
+    def createTopofLegend(self,largestStr):
+        return " "+ "_" * (largestStr + 5)
+    def createCoreOfLegend(self,legendArr,largestStr):
+        legend = legendArr
+        rowCount = 1
+        for col in self.columns:
+            dif = largestStr - len(col.name)
+            row ="|" + col.name
+            row += " " * dif
+            row += " = " +f"{rowCount} |"
+            rowCount += 1
+            legend.append(row)
+        return legend
+    def creatBotOfLegend(self,largestStr):
+        return "|" + "_" * largestStr + "_____|"
+
+    def largestStr(self):
+        largestStr = 0
+        for col in self.columns:
+            if len(col.name) > largestStr:
+                largestStr = len(col.name)
+        return largestStr
+    def printLegend(self):
+        for row in numpy.flip(self.legend):
+            print(row)
+
+if __name__ == "__main__":
+    catsCol = column.Column(20,"Cats")
+    dogCol  = column.Column(15,"Dogs")
+    wormCol = column.Column(12,"Worm")
+    allCol  = [catsCol,dogCol,wormCol]
+    l = Legend(allCol)
+    l.createLegend()
+    l.printLegend()
