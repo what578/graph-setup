48479966609447852adccb84727e7d3d89952332
what578 <ryan_merge@hotmail.com>
2023-01-27 04:21:43 -0600

    inital row methods complete along with method to view graph

--- graph.py
+++ graph.py
@@ -7,6 +7,7 @@ class Graph:
     rows        = None
     columns     = None
     graphName   = None
+    graph       = None
 
 
     def __init__(self,rows,graphName,columns= None):
@@ -32,18 +33,55 @@ class Graph:
         errorRaiser.typeError("setGraphName",name,type("t"))
         self.graphName = name
 
+    def createGraph(self):
+        titleRow = ""
+        bottomLine= ""
+        bottomRow= ""
+        maxValue = self.maxValue()
+        #multiply this by each row to get value scale
+        rowValue = maxValue/self.rows
+        startRows = []
+        largestStr = self.largestRowString(rowValue)
+        for row in range(1,self.rows+1):
+            initalRow = f"{int(row*rowValue)}"
+            dif = largestStr - len(initalRow)
+            initalRow += " " * dif + " |"
+            startRows.append(initalRow)
+        self.graph = startRows
+        return None
+    #we will use maxValue to set the value of the rows 
+    def maxValue(self):
+        max = 0
+        for col in self.columns:
+            if col.value > max:
+                max = col.value
+        return max
+    #knowing the largest row string helps us set the proper amount of spacing between numbers and the graph
+    #ex  notice the differnt amount of " " in the ex largetstRow helps calculate that space
+    # 1  |
+    # 10 |
+    def largestRowString(self,rowValue):
+        largestString = 0
+        for row in range(1,self.rows+1):
+            x = len(str(int(row*rowValue)))
+            if x > largestString:
+                largestString = x
+        return largestString
 
-
-
+    def printGraph(self):
+        for row in self.graph:
+            print(row)
 
 
 
 
 if __name__ == "__main__":
-    rows= 20
+    rows= 25
     graphName = "number of animals"
     catsCol = column.Column(10,"Cats")
     dogCol  = column.Column(20,"Dogs")
     wormCol = column.Column(15,"Worms")
     allCol  = [catsCol,dogCol,wormCol]
     g = Graph(rows,graphName,allCol)
+    g.createGraph()
+    g.printGraph()
